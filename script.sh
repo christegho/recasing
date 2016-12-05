8146eb2ddbf8021c489901662c8f1f36a24bdd5e
C. Tegho <ct506@mlsalt-cpu2.eng.cam.ac.uk>
2016-12-05 16:17:03 +0000

    generating hyps

--- script.sh
+++ script.sh
@@ -20,8 +20,43 @@ fstcompose $DIR/data/ptb/ptb-dev.input.fsts/21.fst toUpper.fst |\
 printstrings -p -m $DIR//data/chars.syms -n 5
 
 #draw
-file=toUpper
+file=lm.char.2
 fstdraw --isymbols=$DIR/data/chars.syms --osymbols=$DIR/data/chars.syms  ${file}.fst  ${file}.dot
 dot -Tjpg  ${file}.dot >  ${file}.jpg
+
+#3.2 Estimating Character-Based Language Models
+#We use the KenLM Toolkit to build a bigram character-based language model:
+$DIR/tools/kenlm/bin/lmplz -o 2 < $DIR/data/train/train.chars > lm.char.2.arpa
+
+#3.3 Building WFSAs from ARPA Language Models
+#We use the Kaldi Toolkit to build a WFSA (an acceptor) from the character bigram language model
+LD_LIBRARY_PATH=$DIR/tools/kaldi/tools/openfst/lib/ \
+$DIR/tools/kaldi/src/lmbin/arpa2fst \
+--read-symbol-table=$DIR/data/chars.syms lm.char.2.arpa - | fstarcsort - > lm.char.2.fst
+fstinfo lm.char.2.fst | head
+
+#3.4  Weighted Transduction from Uncased to Mixed-Case Character Sequences
+#Here we compose the casing WFST toUpper.fst with the character-based language model lm.char.2.fst
+fstdeterminize lm.char.2.fst | fstminimize | fstarcsort | fstcompose toUpper.fst - > toUpper.lm.char.2.fst
+
+#3.5  Generating Recased Hypotheses
+#We can apply the weighted transducer to the original lowercased character string and print the best hypothesis under the language model
+fstcompose $DIR/data/ptb/ptb-dev.input.fsts/21.fst toUpper.lm.char.2.fst |\
+fstproject --project_output | fstshortestpath |\
+printstrings -m $DIR//data/chars.syms -w
+
+#.5.1  Generating the 1-Best WFSAs
+#We process the entire 1700 sentence dev set in batch mode. The following step generates 1700 WFSAs with the best-scoring mixed case character sequence for each sentence.
+mkdir hyps.lm.char.2
+for id in `seq 1 1700`; do
+	fstcompose $DIR/data/ptb/ptb-dev.input.fsts/${id}.fst toUpper.lm.char.2.fst | fstproject --project_output | fstshortestpath > hyps.lm.char.2/${id}.fst
+done
+
+#draw
+for file in toUpper lm.char.2
+do
+fstdraw --isymbols=$DIR/data/chars.syms --osymbols=$DIR/data/chars.syms  ${file}.fst  ${file}.dot
+dot -Tjpg  ${file}.dot >  ${file}.jpg
+done
 
 
